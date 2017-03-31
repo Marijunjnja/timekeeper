f3125a1ad8085841a9a0d575f346eb5998b20de8
Robert Prehn <robert@revelry.co>
2017-03-31 10:04:31 -0500

    Implement basic project list

--- app/controllers/slack_slash_commands_controller.rb
+++ app/controllers/slack_slash_commands_controller.rb
@@ -5,12 +5,48 @@ class CommandWorker
     end
   end
 
+  def tenk_client
+    token = ENV['TENK_TOKEN'] || fail('No api token set. Set TENK_TOKEN environment variable.')
+    @_client ||= Tenk.new token: token, api_base: ENV['TENK_API_BASE']
+  end
+
+  def list_projects
+    projects = []
+    page = 1
+    retval = ''
+
+    loop do
+      response = client.projects.list(page: page)
+      new_projects = response.data
+      projects << new_projects
+      break unless response.paging.next
+      page += 1
+    end
+
+    projects.flatten!
+    project_groups = projects.sort_by(&:name).group_by(&:project_state)
+    project_groups.each do |state, projects|
+      projects.each do |project|
+        retval += "- #{project.name}\n"
+      end
+    end
+
+    retval
+  end
+
   def perform_async(params)
+    command_parts = params[:text].split(' ')
+    report = if command_parts[0] == 'list'
+      list_projects
+    else
+      'Not implemented'
+    end
+
     connection(params).post do |req|
       req.url params[:response_url]
       req.headers['Content-Type'] = 'application/json'
       req.body = JSON.generate({
-        text: "It worked"
+        text: report
       })
     end
   end
