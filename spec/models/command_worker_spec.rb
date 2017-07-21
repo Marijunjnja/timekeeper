2fdfd4c4bb20f969fdea4b84ff40768e1e1c5332
Robert Prehn <robert@revelry.co>
2017-07-20 20:46:01 -0500

    Do not require TENK_TOKEN in tests

--- spec/models/command_worker_spec.rb
+++ spec/models/command_worker_spec.rb
@@ -11,9 +11,10 @@ describe CommandWorker do
     stub_request(:get, "https://api.10000ft.com/api/v1/projects?fields=&filter_field=project_state&filter_list=Confirmed&page=1&per_page=20&with_archived=false&with_phases=false").
       to_return(status: 200, body: '{"data":[{"id":183760,"archived":false,"archived_at":null,"description":null,"guid":"ef3d0b58-eb2e-422a-85c5-0cc669ec167f","name":"Getting the most out of 10,000ft","parent_id":null,"phase_name":null,"project_code":null,"secureurl":"/images/sample_proj_balloons.jpeg","secureurl_expiration":"2017-01-26T22:13:44Z","settings":16,"timeentry_lockout":-1,"ends_at":"2017-05-24","starts_at":"2017-01-25","deleted_at":null,"created_at":"2017-01-25T23:13:44Z","updated_at":"2017-01-26T21:02:24Z","use_parent_bill_rates":false,"thumbnail":"/images/sample_proj_balloons.jpeg","type":"Project","has_pending_updates":false,"client":null,"project_state":"Internal","tags":{"paging":{"self":"/api/v1/projects/183760/tags?per_page=0&page=1","next":null,"previous":null,"page":1,"per_page":0}},"data":[]}], "paging": {}}')
 
+    ClimateControl.modify TENK_TOKEN: '' do
       expect do
         command_worker.perform(params)
       end.to_not raise_error
-
+    end
   end
 end
--- spec/models/commands/people_spec.rb
+++ spec/models/commands/people_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe Commands::People do
   it 'returns a list of people' do
-    command = Commands::People.new
-    data = [
-      OpenStruct.new(display_name: 'tester1', discipline: 'test1'),
-      OpenStruct.new(display_name: 'tester2', discipline: 'test1'),
-      OpenStruct.new(display_name: 'tester3', discipline: 'test2'),
-    ]
-    allow(command.tenk_client.users).to receive(:list).and_return(OpenStruct.new data: data)
-    command.results {}
+    ClimateControl.modify TENK_TOKEN: '' do
+      command = Commands::People.new
+      data = [
+        OpenStruct.new(display_name: 'tester1', discipline: 'test1'),
+        OpenStruct.new(display_name: 'tester2', discipline: 'test1'),
+        OpenStruct.new(display_name: 'tester3', discipline: 'test2'),
+      ]
+      allow(command.tenk_client.users).to receive(:list).and_return(OpenStruct.new data: data)
+      command.results {}
+    end
   end
 end
